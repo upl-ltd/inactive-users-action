217ea95f0382f746aedc3f73be6061baa0231417
Nayana-UPL <142593381+Nayana-UPL@users.noreply.github.com>
2024-03-07 11:25:20 +0530

    Update index.js
    Signed-off-by: Nayana-UPL <142593381+Nayana-UPL@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,66 +1,97 @@
-// const github = require('@actions/github')
-//   , core = require('@actions/core')
-const fs = require('fs')
-  , path = require('path')
-  , core = require('@actions/core')
-  , io = require('@actions/io')
-  , json2csv = require('json2csv')
-  , OrganizationActivity = require('./src/OrganizationUserActivity')
-  , githubClient = require('./src/github/githubClient')
-  , dateUtil = require('./src/dateUtil')
-;
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+const core = require('@actions/core');
+const io = require('@actions/io');
+const json2csv = require('json2csv');
+const OrganizationActivity = require('./src/OrganizationUserActivity');
+const githubClient = require('./src/github/githubClient');
+const dateUtil = require('./src/dateUtil');
+
+async function getSSOEmails(token) {
+  const response = await axios({
+    url: 'https://api.github.com/graphql',
+    method: 'post',
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    },
+    data: {
+      query: `
+        {
+          enterprise(slug: "upl") {
+            ownerInfo {
+              samlIdentityProvider {
+                externalIdentities(after: null, first: 100) {
+                  edges {
+                    node {
+                      samlIdentity {
+                        nameId
+                      }
+                      user {
+                        login
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      `,
+    },
+  });
+
+  return response.data.data.enterprise.ownerInfo.samlIdentityProvider.externalIdentities.edges.map(edge => {
+    return {
+      ssoEmail: edge.node.samlIdentity.nameId,
+      username: edge.node.user.login,
+    };
+  });
+}
 
 async function run() {
-  const since = core.getInput('since')
-    , days = core.getInput('activity_days')
-    , token = getRequiredInput('token')
-    , outputDir = getRequiredInput('outputDir')
-    , organization = getRequiredInput('organization')
-    , maxRetries = getRequiredInput('octokit_max_retries')
-  ;
+  const since = core.getInput('since');
+  const days = core.getInput('activity_days');
+  const token = getRequiredInput('token');
+  const outputDir = getRequiredInput('outputDir');
+  const organization = getRequiredInput('organization');
+  const maxRetries = getRequiredInput('octokit_max_retries');
 
   let fromDate;
   if (since) {
-    console.log(`Since Date has been specified, using that instead of active_days`)
+    console.log(`Since Date has been specified, using that instead of active_days`);
     fromDate = dateUtil.getFromDate(since);
   } else {
     fromDate = dateUtil.convertDaysToDate(days);
   }
 
-  // Ensure that the output directory exists before we our limited API usage
-  await io.mkdirP(outputDir)
+  await io.mkdirP(outputDir);
 
-  const octokit = githubClient.create(token, maxRetries)
-    , orgActivity = new OrganizationActivity(octokit)
-  ;
+  const octokit = githubClient.create(token, maxRetries);
+  const orgActivity = new OrganizationActivity(octokit);
 
   console.log(`Attempting to generate organization user activity data, this could take some time...`);
   const userActivity = await orgActivity.getUserActivity(organization, fromDate);
-  const ssoClient = require('sso-client-library');
-  for (const activity of userActivity) {
-    const githubUsername = activity.jsonPayload.actor.login;
 
-  try {
-      const ssoUserDetails = await ssoClient.getUserDetails(githubUsername);
-      const ssoUserEmail = ssoUserDetails.email;
-    activity.jsonPayload.email = ssoUserEmail;
-    } catch (error) {
-      console.error(`Failed to fetch SSO details for ${githubUsername}: ${error.message}`);
-    }
-  }
-  saveIntermediateData(outputDir, userActivity.map(activity => activity.jsonPayload));
+  console.log(`User activity data captured, fetching SSO emails...`);
+  const ssoEmails = await getSSOEmails(token);
 
-  // Convert the JavaScript objects into a JSON payload so it can be output
-  console.log(`User activity data captured, generating report...`);
-  const data = userActivity.map(activity => activity.jsonPayload)
-    , csv = json2csv.parse(data, {})
-  ;
+  const data = userActivity.map(activity => {
+    const ssoEmail = ssoEmails.find(email => email.username === activity.jsonPayload.username);
+    return {
+      ...activity.jsonPayload,
+      ssoEmail: ssoEmail ? ssoEmail.ssoEmail : 'N/A',
+    };
+  });
+
+  console.log(`Data captured, generating report...`);
+  const csv = json2csv.parse(data, {});
 
   const file = path.join(outputDir, 'organization_user_activity.csv');
   fs.writeFileSync(file, csv);
   console.log(`User Activity Report Generated: ${file}`);
 
-  // Expose the output csv file
   core.setOutput('report_csv', file);
 }
 
@@ -71,19 +102,9 @@ async function execute() {
     core.setFailed(err.message);
   }
 }
-execute();
 
+execute();
 
 function getRequiredInput(name) {
   return core.getInput(name, {required: true});
-}
-
-function saveIntermediateData(directory, data) {
-  try {
-    const file = path.join(directory, 'organization_user_activity.json');
-    fs.writeFileSync(file, JSON.stringify(data));
-    core.setOutput('report_json', file);
-  } catch (err) {
-    console.error(`Failed to save intermediate data: ${err}`);
-  }
 }
